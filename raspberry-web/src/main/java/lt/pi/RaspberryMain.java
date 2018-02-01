3ceeadaf1763ff77dc7df8f67fc657c26acee975
babonis <babonis@gmail.com>
2018-02-01 23:12:02 +0200

    pradžia

--- raspberry-web/src/main/java/lt/pi/RaspberryMain.java
+++ raspberry-web/src/main/java/lt/pi/RaspberryMain.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RaspberryMain {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }

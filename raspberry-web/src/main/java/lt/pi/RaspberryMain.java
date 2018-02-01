787e955a739367d700d3fd247be2690bfc1a82fc
arvydasm <babonis@gmail.com>
2018-02-01 21:56:42 +0200

    Add files via upload

---
+++ raspberry-web/src/main/java/lt/pi/RaspberryMain.java
@@ -0,0 +1,14 @@
+package lt.pi;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

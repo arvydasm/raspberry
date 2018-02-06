8471ee4f145c9eb8d976f67ac67a4af765708e9d
babonis <babonis@gmail.com>
2018-02-06 22:57:43 +0200

    pradžia

--- raspberry-gpio/src/main/java/lt/pi/GPIOService.java
+++ raspberry-gpio/src/main/java/lt/pi/GPIOService.java
@@ -1,18 +1,38 @@
 package lt.pi;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class GPIOService {
+
+
 
     // create gpio controller
     final private GpioController gpio = GpioFactory.getInstance();
 
-    GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Light", PinState.LOW);
+
+
+    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Light", PinState.LOW);
+
+    final GpioPinDigitalInput input = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+    public GPIOService() {
+        addListeners();
+    }
+
 
     public void light(){
         pin.toggle();
     }
 
+    private void addListeners() {
+        input.addListener((GpioPinListenerDigital)
+                (GpioPinDigitalStateChangeEvent event)	->	{
+                    //	set	output	state	to	match	the	input	state
+                    light();
+                });
+    }
 
 
 }
